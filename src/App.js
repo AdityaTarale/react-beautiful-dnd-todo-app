597888962dc76bb4668f9b6980da720653b7d586
AdityaTarale <tarale.adi@gmail.com>
2021-07-06 10:20:45 +0530

    Create and style a list of data with react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import initialData from "./initial-data";
+import Column from "./components/Column";
 
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState(initialData);
+
+  return (
+    <div className="App">
+      {state.columnOrder.map((columnId) => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
